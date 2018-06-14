ce5e6d20e3950f27e360878792965b8c1e1d5945
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-14 12:57:33 +0700

    [ADD] master : create default layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        
+        {{-- custom_head --}}
+        @yield('custom_head')
+    </head>
+
+    <body>
+        {{-- content --}}
+        @yield('content')
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+        {{-- custom_script --}}
+        @yield('custom_script')
+
+    </body>
+</html>
