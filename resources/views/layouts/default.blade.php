c28a1b366cbbfe0c78205f8c30b3b5d4bff1c1c0
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-14 21:48:17 +0700

    [ADD] master : add navbar and change path bootstrap

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,18 +3,19 @@
         <title>@yield('title')</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        
+        <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
+
         {{-- custom_head --}}
         @yield('custom_head')
     </head>
 
     <body>
+        @include('utils.navbar')
         {{-- content --}}
         @yield('content')
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="{{ asset('/js/jquery-3.3.1.min.js') }}"></script>
+        <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
         
         {{-- custom_script --}}
         @yield('custom_script')
--- resources/views/utils/navbar.blade.php
+++ resources/views/utils/navbar.blade.php
@@ -1,3 +1,57 @@
 <section id="navbar">
-    
+    <div class="container-fluid">
+        <div class="row justify-content-center d-none d-lg-block navbar-bg line paddingTB18">
+            <div class="col-12 text-center">
+                <img src="{{ asset('/imgs/logo_sansiri.png') }}" alt="Sansiri Logo" width="62px">
+            </div>
+        </div>
+        <div class="row justify-content-around navbar-bg">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">
+                    <img class="logo-size" src="{{ asset('/imgs/logo_kawahaus.png') }}" alt="KAWA Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link menu" href="#">หน้าหลัก <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">แนวคิดโครงการ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">โปรเจคไฮไลค์</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">แบบแปลน</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">แกลเลอรี่</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">ที่ตั้งโครงการ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">ลงทะเบียนรับข้อมูลเพิ่มเติม</a>
+                        </li>
+                        <li class="nav-item float-right">
+                            <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown">TH
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">TH</a></li>
+                                        <li><a href="#">EN</a></li>
+                                    </ul>
+                                </button>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu" href="#">แชร์</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
 </section>
