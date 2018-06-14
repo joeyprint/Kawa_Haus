28b5e4cc1b7e050a317659cba2763ec8d34f716a
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-14 22:00:05 +0700

    [ADD] master : add font awesome into project

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/fontawesome.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
 
         {{-- custom_head --}}
@@ -12,13 +13,17 @@
 
     <body>
         @include('utils.navbar')
+
         {{-- content --}}
         @yield('content')
+
         <script src="{{ asset('/js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('/js/fontawesome.min.js') }}"></script>
         
         {{-- custom_script --}}
         @yield('custom_script')
 
+        @include('utils.footer')
     </body>
 </html>
