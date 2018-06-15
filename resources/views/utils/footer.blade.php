294a8ebe884a52debb62f1e4506cf0565f30ecdd
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-15 11:00:16 +0700

    [ADD] master : create footer into index page

---
+++ resources/views/utils/footer.blade.php
@@ -0,0 +1,87 @@
+<section id="footer">
+    <div class="container-fluid footer-bg">
+        <div class="row">
+            <div class="col-12 text-center padding-allproject">
+                <p class="text no-margin">โครงการทั้งหมด</p>
+                {{-- <img src="{{ asset('/imgs/arrow.png') }}" alt="down arrow"> --}}
+            </div>
+        </div>
+        <div class="row d-lg-none text-center">
+            <div class="col-12 padding-follow">
+                <p class="text-follow no-margin">Follow us</p>
+            </div>
+            <div class="col-12 fa-icon text-center">
+                <div class="d-flex flex-row justify-content-center">
+                    <i class="fab padding-icon fa-facebook-f"></i>
+                    <i class="fab padding-icon fa-twitter"></i>
+                    <i class="fab padding-icon fa-youtube"></i>
+                </div>
+            </div>
+            <div class="col-12 fa-icon text-center">
+                <div class="d-flex flex-row justify-content-center">
+                    <i class="fab padding-icon fa-instagram"></i>
+                    <i class="fab padding-icon fa-pinterest"></i>
+                    <i class="fab padding-icon fa-google-plus-g"></i>
+                </div>
+            </div>
+            <div class="col-12 text-center">
+                <img class="img-callcenter" src="{{ asset('/imgs/call-center.svg') }}" alt="call center">
+            </div>
+        </div>
+        <div class="row d-none d-lg-block border-line">
+            <div class="d-flex flex-row justify-content-start align-items-center w-100">
+                <div class="col-lg-6">
+                        <img class="img-callcenter" src="{{ asset('/imgs/call-center.svg') }}" alt="call center">
+                </div>
+                <div class="col-lg-6 fa-icon text-right">
+                    <span class="text-follow padding-follow no-margin">Follow us</span>
+                    <i class="fab fa-lg padding-icon fa-facebook-f"></i>
+                    <i class="fab fa-lg padding-icon fa-twitter"></i>
+                    <i class="fab fa-lg padding-icon fa-youtube"></i>
+                    <i class="fab fa-lg padding-icon fa-instagram"></i>
+                    <i class="fab fa-lg padding-icon fa-pinterest"></i>
+                    <i class="fab fa-lg padding-icon fa-google-plus-g"></i>
+                </div>
+            </div>
+        </div>
+        <div class="row d-none d-lg-block">
+            <div class="d-flex flex-row justify-content-start">
+                <div class="block">
+                    <p class="sub-text text-uppercase">Sansiri call centre</p>
+                    <p class="main-text font-weight-bold text-uppercase">Live Chat</p>
+                </div>
+                <div class="block">
+                    <p class="sub-text text-uppercase">Sansiri</p>
+                    <p class="main-text font-weight-bold text-uppercase">Family</p>
+                </div>
+                <div class="block">
+                    <p class="sub-text text-uppercase">Sansiri</p>
+                    <p class="main-text font-weight-bold text-uppercase">Blog</p>
+                </div>
+                <div class="block">
+                    <p class="sub-text text-uppercase">Sansiri International</p>
+                    <p class="main-text font-weight-bold text-uppercase">Buyer Guide</p>
+                </div>
+            </div>
+        </div>
+        <div class="row d-none d-lg-block">
+            <div class="d-flex flex-row justify-content-start">
+                <div class="apply-block">
+                    <p class="no-margin">ติดต่อเรา</p>
+                </div>
+                <div class="apply-block">
+                    <p class="no-margin">สมัครงาน</p>
+                </div>
+            </div>
+        </div>
+        <div class="row d-none d-lg-block">
+            <div class="d-flex flex-row justify-content-start">
+                <div class="col-lg-6">
+                    <p class="footer-des">COPYRIGHT © 2014 , SANSIRI PUBLIC CO.,LTD ALL RIGHTS RESERVED. | Sansiri.com<br>
+                    ภาพที่ปรากฏบนเว็บไซต์ ประกอบด้วย ภาพที่ถ่ายจากสถานที่จริง, ภาพจริงบรรยากาศจำลอง และภาพจำลอง<br>
+                    หากมีข้อสงสัยเกี่ยวกับภาพใด ๆ สามารถสอบถามจากเจ้าหน้าที่ หรือเข้าเยี่ยมชมสถานที่จริง ณ โครงการ โทร. 1685</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
