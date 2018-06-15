aee417293125223f8e5d3cc07e08cee8c26c4713
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-15 14:49:56 +0700

    [ADD] master : add banner into index view

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,46 @@
+<section id="banner">
+    <div class="container-fluid no-padding">
+        <div class="row no-gutters banner-block d-block d-lg-none">
+            <div class="col-12 img-block">
+                <img class="img-banner" src="{{ asset('/imgs/banner.png') }}" alt="banner" width="100%">
+            </div>
+            <div class="col-12 text-center concept">
+                <p class="kawa-text concept-text no-margin">KAWA HAUS</p>
+                <p class="sub-text concept-text no-margin">อ่อนนุช T77<br>LIVE A GOOD STORY</p>
+                <div class="line"></div>
+                <p class="des-text concept-text no-margin">ท่ามกลางความเร่งรีบของคนเมือง ธรรมชาติยังเป็นสิ่งหนึ่งที่ช่วย<br>
+                    สร้างเรื่องราวดีๆ ของชีวิต ภายใต้ร่มเงาของต้นไม้ใหญ่ และความ<br>
+                    สดชื่นของชีวิตริมน้ำในขณะที่ทุกความสะดวกสบายอยู่ใกล้แค่เอื้อม</p>
+            </div>
+            <div class="bg-price text-center">
+                <p class="concept-text no-margin padding-price price-text">เริ่ม 3.59 ล้าน*</p>
+            </div>
+            <div class="bg-regis text-center regis-pos">
+                <p class="concept-text no-margin padding-price price-text">REGISTER</p>
+            </div>
+        </div>
+        <div class="row no-gutters d-none d-lg-block banner-block">
+            <div class="col-12 img-block">
+                <img class="img-banner" src="{{ asset('/imgs/banner.png') }}" alt="banner" width="100%">
+            </div>
+        </div>
+        <div class="row no-gutters concept-box d-none d-lg-block">
+            <div class="flex-column w-100">
+                <div class="text-center concept">
+                    <p class="kawa-text concept-text no-margin">KAWA HAUS</p>
+                    <p class="sub-text concept-text no-margin">อ่อนนุช T77<br>LIVE A GOOD STORY</p>
+                    <div class="line"></div>
+                    <p class="des-text concept-text no-margin">ท่ามกลางความเร่งรีบของคนเมือง ธรรมชาติยังเป็นสิ่งหนึ่งที่ช่วย<br>
+                        สร้างเรื่องราวดีๆ ของชีวิต ภายใต้ร่มเงาของต้นไม้ใหญ่ และความ<br>
+                        สดชื่นของชีวิตริมน้ำในขณะที่ทุกความสะดวกสบายอยู่ใกล้แค่เอื้อม</p>
+                </div>
+                <div class="bg-price text-center">
+                    <p class="concept-text no-margin padding-price price-text">เริ่ม 3.59 ล้าน*</p>
+                </div>
+                <div class="bg-regis text-center regis-pos">
+                    <p class="concept-text no-margin padding-price price-text">REGISTER</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,14 @@
     Sansiri Oka
 @endsection
 
+@section('custom_head')
+    <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
+@endsection
+
 @section('content')
+    @include('components.banner')
+@endsection
+
+@section('custom_script')
 
 @endsection
