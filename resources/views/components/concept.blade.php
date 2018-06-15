d9627c41d1db16ea7c8ccf405cafdc7fd9d8f916
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-15 17:49:05 +0700

    [ADD] master : create concept component into index view

---
+++ resources/views/components/concept.blade.php
@@ -0,0 +1,36 @@
+<section id="concept">
+    <div class="container concept-area">
+        <div class="row">
+            <div class="col-lg-1 d-none d-lg-block"></div>
+            <div class="col-12 col-lg-4">
+                <div class="row">
+                    <div class="d-flex justify-content-start w-100">
+                        <div class="col-10 col-lg-7 topic-box">
+                            <p class="topic-text font-weight-bold no-margin">แนวคิดโครงการ</p>
+                            <p class="kawa-text no-margin">KAWA</p>
+                            <img class="padding-wave-img" src="{{ asset('/imgs/waves.png') }}" alt="wave kava">
+                        </div>
+                    </div>
+                </div>
+                <div class="row img-concept-pos">
+                    <div class="col-12 col-lg-10">
+                        <img src="{{ asset('/imgs/31-full.jpg') }}" alt="image for concept" width="100%">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-10 col-lg-7 line"></div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 story-box">
+                <p class="story-text font-weight-bold">LIVE A GOOD STORY</p>
+                <p class="des-text text-center">ท่ามกลางความเร่งรีบของคนเมืองธรรมชาติยังเป็นสิ่งหนึ่งที่ช่วยสร้างเรื่องราวดีๆของ
+                    ชีวิตภายใต้ร่มเงาของต้นไม้ใหญ่และความสดชื่นของชีวิตริมน้ำในขณะที่ทุกความสะดวก
+                    สบายอยู่ใกล้แค่เอื้อมท่ามกลางความเร่งรีบของคนเมืองธรรมชาติยังเป็นสิ่งหนึ่งที่ช่วย
+                    สร้างเรื่องราวดีๆ</p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <img class="d-none d-lg-block water-mask-right" src="{{ asset('/imgs/kawa_water.png') }}" alt="water mask">
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,12 @@
 
 @section('custom_head')
     <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/concept.css') }}">
 @endsection
 
 @section('content')
     @include('components.banner')
+    @include('components.concept')
 @endsection
 
 @section('custom_script')
