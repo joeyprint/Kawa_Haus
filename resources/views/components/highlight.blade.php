94f49cf0052bd8a81dbf49fb2c5c1daaef586fe4
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-18 14:20:42 +0700

    [ADD] master : create highlight component and style

---
+++ resources/views/components/highlight.blade.php
@@ -0,0 +1,48 @@
+<section id="highlight">
+    <div class="container-fluid no-padding">
+        <div class="row no-gutters">
+            <div class="col-12">
+                <p class="topic-text text-uppercase font-weight-bold">Project Highlight</p>
+            </div>
+        </div>
+        <div class="row box no-margin">
+            <div class="col-12 all-box no-padding">
+                <div class="menu-box">
+                    <p class="text-uppercase no-margin subtopic-text">lobby</p>
+                    <p class="text-uppercase no-margin seemore-text">
+                        see more <img class="seemore-icon" src="{{ asset('/imgs/arrow_right_black.png') }}" alt="see more icon">
+                    </p>
+                </div>
+                <div class="line"></div>
+                <div class="menu-box">
+                    <p class="text-uppercase no-margin subtopic-text">spacious outdoor common area</p>
+                    <p class="text-uppercase no-margin seemore-text">
+                        see more <img class="seemore-icon" src="{{ asset('/imgs/arrow_right_black.png') }}" alt="see more icon">
+                    </p>
+                </div>
+                <div class="line"></div>
+                <div class="menu-box">
+                    <p class="text-uppercase no-margin subtopic-text">bamboo forest trial</p>
+                    <p class="text-uppercase no-margin seemore-text">
+                        see more <img class="seemore-icon" src="{{ asset('/imgs/arrow_right_black.png') }}" alt="see more icon">
+                    </p>
+                </div>
+                <div class="line"></div>
+                <div class="menu-box">
+                    <p class="text-uppercase no-margin subtopic-text">co-working space</p>
+                    <p class="text-uppercase no-margin seemore-text">
+                        see more <img class="seemore-icon" src="{{ asset('/imgs/arrow_right_black.png') }}" alt="see more icon">
+                    </p>
+                </div>
+                <div class="line"></div>
+                <div class="menu-box">
+                    <p class="text-uppercase no-margin subtopic-text">co-kitchen space</p>
+                    <p class="text-uppercase no-margin seemore-text">
+                        see more <img class="seemore-icon" src="{{ asset('/imgs/arrow_right_black.png') }}" alt="see more icon">
+                    </p>
+                </div>
+                <div class="line"></div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,14 @@
     <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/concept.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/regis.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/highlight.css') }}">
 @endsection
 
 @section('content')
     @include('components.banner')
     @include('components.concept')
     @include('components.regis')
+    @include('components.highlight')
 @endsection
 
 @section('custom_script')
