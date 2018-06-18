7b0829fa673585aff74676d9841e6e98fcd6235c
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-18 10:45:59 +0700

    [ADD] master : create register component and register style

---
+++ resources/views/components/regis.blade.php
@@ -0,0 +1,74 @@
+<section id="regis">
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            <div class="col-12">
+                <p class="topic-text no-margin">ลงทะเบียนรับส่วนลดสูงสุด 50,000 บาท*</p>
+            </div>
+            <div class="col-12">
+                <p class="kawa-text no-margin">KAWA HAUS อ่อนนุช T77</p>
+            </div>
+            <div class="col-12">
+                <p class="call-text no-margin">Call Centre 1685<br class="d-block d-lg-none"><span class="d-none d-lg-inline"> | </span>kawahaus@sansiri.com</p>
+            </div>
+        </div>
+        <div class="row justify-content-center agree-box">
+            <div class="col-11 col-lg-8">
+                <input type="checkbox" class="checkbox">
+                <span class="agree-text">I DON'T HAVE A PERMANENT ADDRESS IN THAILAND.</span>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <form action="" class="w-100">
+                <div class="col-lg-2 d-none d-lg-block"></div>
+                <div class="col-12 col-lg-4 no-margin">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="ชื่อ">
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 no-margin">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="นามสกุล">
+                    </div>                    
+                </div>
+                <div class="col-lg-2 d-none d-lg-block"></div>
+                <div class="col-12 col-lg-8 no-margin">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="เบอร์โทรศัพท์">
+                    </div>
+                </div>                    
+                <div class="col-12 col-lg-8 no-margin">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="อีเมล">
+                    </div>                    
+                </div>                    
+                <div class="col-12 col-lg-4 no-margin">
+                    <div class="form-group">
+                        <select class="select-box align-middle" name="province" id="province">
+                            <option value="none">ระบุจังหวัดที่ท่านอาศัยอยู่</option>
+                        </select>
+                    </div>                    
+                </div>
+                <div class="col-12 col-lg-4 no-margin">
+                    <div class="form-group">
+                        <select class="select-box align-middle" name="district" id="district">
+                            <option value="none">ระบุอำเภอที่ท่านอาศัยอยู่</option>
+                        </select>
+                    </div>                    
+                </div>
+                <div class="col-12 col-lg-4 no-margin">
+                    <div class="form-group">
+                        <select class="select-box align-middle" name="budget" id="budget">
+                            <option value="none">งบประมาณในการซื้อ</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-8">
+                    <button class="submit-btn font-weight-bold align-middle" type="submit">ส่ง</button>
+                </div>
+            </form>
+        </div>
+        <div>
+            <img class="d-none d-lg-block water-mask-left" src="{{('/imgs/kawa_water.png')}}" alt="water mask">
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,13 @@
 @section('custom_head')
     <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/concept.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/regis.css') }}">
 @endsection
 
 @section('content')
     @include('components.banner')
     @include('components.concept')
+    @include('components.regis')
 @endsection
 
 @section('custom_script')
