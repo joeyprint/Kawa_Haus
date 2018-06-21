6fc899d678959dbc94d1bc089832ffd875690f14
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-21 10:16:32 +0700

    [ADD] master : add owl carousel into gallery

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -13,26 +13,154 @@
             <div class="col-12 col-lg-7 img-pos-mobile img-pos-left">
                 <div class="row no-gutters">
                     <div class="col-6">
-                        <div class="block1 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/1.jpg') }}" alt="1">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block1 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/1.jpg') }}" alt="1">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block1 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/9.jpg') }}" alt="9">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block1 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/17.jpg') }}" alt="17">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block1 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/25.jpg') }}" alt="25">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-6">
-                        <div class="block2 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/2.jpg') }}" alt="2">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block2 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/2.jpg') }}" alt="2">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block2 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/10.jpg') }}" alt="10">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block2 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/18.jpg') }}" alt="18">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block2 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/26.jpg') }}" alt="26">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                     <img class="d-none d-lg-block chill-icon" src="{{ asset('/imgs/chill-icon-desktop.jpg') }}" alt="">
                 </div>
                 <div class="row no-gutters">
                     <div class="col-6">
-                        <div class="block3 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/3.jpg') }}" alt="3">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block3 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/3.jpg') }}" alt="3">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block3 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/11.jpg') }}" alt="11">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block3 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/19.jpg') }}" alt="19">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block3 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/27.jpg') }}" alt="27">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-6">
-                        <div class="block4 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/4.jpg') }}" alt="4">
+                    <div class="col-6 col-lg-4">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block4 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/4.jpg') }}" alt="4">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block4 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/12.jpg') }}" alt="12">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block4 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/20.jpg') }}" alt="20">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block4 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/28.jpg') }}" alt="28">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -54,25 +182,153 @@
                 </div>
                 <div class="row no-gutters">
                     <div class="col-6 col-lg-7">
-                        <div class="block5 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/5.jpg') }}" alt="5">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block5 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/5.jpg') }}" alt="5">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block5 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/13.jpg') }}" alt="13">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block5 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/21.jpg') }}" alt="21">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block5 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/29.jpg') }}" alt="29">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-6 col-lg-5">
-                        <div class="block6 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/6.jpg') }}" alt="6">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block6 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/6.jpg') }}" alt="6">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block6 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/14.jpg') }}" alt="14">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block6 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/22.jpg') }}" alt="22">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block6 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/30.jpg') }}" alt="30">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="row no-gutters">
                     <div class="col-3 col-lg-4">
-                        <div class="block7 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/7.jpg') }}" alt="7">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block7 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/7.jpg') }}" alt="7">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block7 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/15.jpg') }}" alt="15">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block7 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/23.jpg') }}" alt="23">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block7 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/31.jpg') }}" alt="31">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-5 col-lg-5">
-                        <div class="block8 overflow-hid">
-                            <img class="img-size" src="{{ asset('/imgs/8.jpg') }}" alt="8">
+                        <div class="owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="block8 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/8.jpg') }}" alt="8">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block8 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/16.jpg') }}" alt="16">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block8 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/24.jpg') }}" alt="24">
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="block8 overflow-hid">
+                                            <img class="img-size" src="{{ asset('/imgs/32.jpg') }}" alt="32">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots disabled">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
                         </div>
                     </div>
                 </div>
