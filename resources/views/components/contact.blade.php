98e80689b8a35c53d7f27c356587488144324b24
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-21 14:20:42 +0700

    [ADD] master : create contact component into project

---
+++ resources/views/components/contact.blade.php
@@ -0,0 +1,89 @@
+<section id="contact">
+    <div class="container">
+        <div class="row justify-content-center d-block d-lg-none">
+            <div class="col-12 text-center">
+                <img class="img-kawa-size" src="{{ asset('/imgs/logo_wave.png') }}" alt="kawa">
+                <span class="kawa-text">KAWA</span>
+            </div>
+            <div class="line"></div>
+            <div class="col-12 text-center">
+                <p class="topic-text">ที่ตั้งโครงการ</p>
+            </div>
+            <div class="col-12 text-center">
+                <p class="subtopic-text">อ่อนนุช T77</p>
+            </div>
+            <div class="col-12">
+                <p class="des-text">
+                    เพียง 2 นาทีจากทางด่วน และ 5 นาทีจาก BTS อ่อนนุช T77 มีทุกสิ่งที่คุณต้องการใกล้ทุกความสะดวก
+                    สบาย ทั้ง ฮาบิโตะ มอลล์, โรงเรียนนานาชาติ บางกอก เพรพ
+                </p>
+            </div>
+            <div class="col-12">
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <button class="btn btn-contact w-100">
+                            <i class="fas fa-map-marker fa-xs"></i>&nbsp;&nbsp;&nbsp;
+                            <span class="btn-text">GOOGLE MAP</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <button class="btn btn-contact w-100">
+                            <i class="fas fa-location-arrow fa-xs"></i>&nbsp;&nbsp;&nbsp;
+                            <span class="btn-text">GET DIRECTION</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row d-none d-lg-block">
+            <div class="d-flex flex-row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="col-lg-12 text-center">
+                        <p class="topic-text">ที่ตั้งโครงการ</p>
+                    </div>
+                    <div class="col-lg-12 text-center">
+                        <p class="subtopic-text no-margin">อ่อนนุช T77</p>
+                    </div>
+                    <div class="col-lg-12">
+                        <p class="des-text">
+                            เพียง 2 นาทีจากทางด่วน และ 5 นาทีจาก BTS อ่อนนุช T77 มีทุกสิ่งที่คุณต้องการใกล้ทุกความสะดวก
+                            สบาย ทั้ง ฮาบิโตะ มอลล์, โรงเรียนนานาชาติ บางกอก เพรพ
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div class="col-lg-12 text-right">
+                            <img class="img-kawa-size" src="{{ asset('/imgs/logo_wave.png') }}" alt="kawa">
+                            <span class="kawa-text">KAWA</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 block-line">
+                            <div class="line"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row justify-content-center block-btn">
+                                <div class="col-6 text-right btn-contact">
+                                    <i class="fas fa-map-marker fa-xs"></i>&nbsp;&nbsp;&nbsp;
+                                    <span class="btn-text">GOOGLE MAP</span>
+                                </div>
+                                <div class="col-6 text-right btn-contact">
+                                    <i class="fas fa-location-arrow fa-xs"></i>&nbsp;&nbsp;&nbsp;
+                                    <span class="btn-text">GET DIRECTION</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid no-padding">
+        <div id="map"></div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset('/css/plan.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/video.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/gallery.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/contact.css') }}">
 @endsection
 
 @section('content')
@@ -24,9 +25,20 @@
     @include('components.plan')
     @include('components.video')
     @include('components.gallery')
+    @include('components.contact')
 @endsection
 
 @section('custom_script')
     <script src="{{ asset('/js/plan.js') }}"></script>
     <script src="{{ asset('/js/gallery.js') }}"></script>
+    <script>
+        var map;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 13.714667, lng: 100.604528},
+            zoom: 8
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
 @endsection
